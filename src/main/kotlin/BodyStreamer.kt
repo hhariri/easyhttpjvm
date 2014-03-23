5eb1b104b03964b9b2dc42f2af866b4c762d10c3
Hadi Hariri <mail@hadihariri.com>
2014-03-23 21:36:14 +0100

    fixed streamer for text/plain, text/html

--- src/main/kotlin/BodyStreamer.kt
+++ src/main/kotlin/BodyStreamer.kt
@@ -9,7 +9,7 @@ import io.netty.buffer.ByteBuf
 import io.netty.buffer.Unpooled
 import io.netty.util.CharsetUtil
 
-public class BodyStreamer : Streamer("application/json", "application/json;charset=\\w*\\W*\\w*") {
+public class BodyStreamer : Streamer("text/plain", "text/html", "application/json", "application/json;charset=\\w*\\W*\\w*") {
     override fun streamData(data: Any?, method: HttpMethod, rawPath: String): StreamerResponse {
         val buffer = Unpooled.copiedBuffer(data.toString(), CharsetUtil.UTF_8)
         return StreamerResponse(DefaultFullHttpRequest(HttpVersion.HTTP_1_1, method, rawPath, buffer), data.toString().length)
