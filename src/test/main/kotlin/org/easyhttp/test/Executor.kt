00fb88789f93cbb646ac861707fe851c236e489e
Hadi Hariri <mail@hadihariri.com>
2014-04-25 12:42:40 +0200

    Some changes for POST

--- src/test/main/kotlin/org/easyhttp/test/Executor.kt
+++ src/test/main/kotlin/org/easyhttp/test/Executor.kt
@@ -1,26 +1,14 @@
 package org.easyHttp.test
 
 import org.easyHttp.EasyHttp
+import rx.Subscriber
+import org.easyHttp.Response
+import rx.Observable
 
 
 fun main(args: Array<String>) {
 
-    val http = EasyHttp()
 
-    http.get("http://www.google.com").toBlockingObservable()?.forEach({
-
-        println(it?.content)
-    })
-    http.get("http://www.google.com").toBlockingObservable()?.forEach({
-        println(it?.content)
-    })
-    //subscribe( {
-   //     println(it?.content)
-    //})
-   /* http.get("http:/www.google.com").subscribe( {
-
- //       println(it?.content)
-    })*/
 
 
 
