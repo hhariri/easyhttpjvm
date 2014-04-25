c505a6ccc3d5ad657acecf978195b33949d8a629
Hadi Hariri <mail@hadihariri.com>
2014-04-25 12:50:59 +0200

    checks

--- src/test/main/kotlin/org/easyhttp/test/Executor.kt
+++ src/test/main/kotlin/org/easyhttp/test/Executor.kt
@@ -8,6 +8,21 @@ import rx.Observable
 
 fun main(args: Array<String>) {
 
+    val http = EasyHttp()
+
+
+
+
+
+
+    val o1 = http.get("http://www.google.com")
+    val o2 = http.get("http://www.google.com")
+
+    Observable.merge(o1, o2)?.subscribe() {
+        http.get("http://www.gooole.es").subscribe() {
+            println(it?.content)
+        }
+    }
 
 
 
