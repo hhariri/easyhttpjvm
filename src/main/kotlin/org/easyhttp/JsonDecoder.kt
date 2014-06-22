adb8c915e8aa5cd483897cf8a025c216c904fdb2
Hadi Hariri <mail@hadihariri.com>
2014-06-22 16:33:50 +0200

    Fixed regular expression for encoding/decoding have space between media type and charset

--- src/main/kotlin/org/easyhttp/JsonDecoder.kt
+++ src/main/kotlin/org/easyhttp/JsonDecoder.kt
@@ -3,7 +3,7 @@ package org.easyHttp
 import com.google.gson.Gson
 
 
-public class JsonDecoder : ContentDecoder("application/json", "application/json;charset=\\w*\\W*\\w*") {
+public class JsonDecoder : ContentDecoder("application/json", "application/json;\\s*charset=\\w*\\W*\\w*") {
     override fun decode<T>(input: String, objectType: Class<T>): T {
         val gson = Gson()
         return (gson.fromJson<T>(input, objectType) as T)
--- src/main/kotlin/org/easyhttp/JsonEncoder.kt
+++ src/main/kotlin/org/easyhttp/JsonEncoder.kt
@@ -5,7 +5,7 @@ import io.netty.handler.codec.http.multipart.HttpPostRequestEncoder
 import com.google.gson.Gson
 
 
-public class JsonEncoder : ContentEncoder("application/json", "application/json;charset=\\w*\\W*\\w*") {
+public class JsonEncoder : ContentEncoder("application/json", "application/json;\\s*charset=\\w*\\W*\\w*") {
     override fun encode(data: Any?): Any {
         if (data is String) {
             return data
